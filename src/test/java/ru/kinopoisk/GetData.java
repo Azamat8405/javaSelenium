487636324f363af20780e3f5636e86160e16ecdc
azam <azamat8405@yandex.ru>
2020-06-20 14:53:32 +0300

    1

--- src/test/java/ru/kinopoisk/GetData.java
+++ src/test/java/ru/kinopoisk/GetData.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.*;
+import java.net.*;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -15,41 +16,101 @@ public class GetData {
 
         try {
 
-            System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32_75\\chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32_83\\chromedriver.exe");
             ChromeDriver driver = new ChromeDriver();
 
-            for(int i = 3885; i <= 354000; i++ ){
 
-//                if(i > 00){
-//                    break;
-//                }
-//                if(i%50 == 0){
-//                    Thread.sleep(60000); // 60 секунд
-//                }
-//                if(i%100 == 0){
-//                    Thread.sleep(240000); // 240 секунд
-//                }
-//                if(i%502 == 0){
-//                    Thread.sleep(500000); // 240 секунд
-//                }
+            driver.get("https://www.instagram.com/");
+            Thread.sleep(4000);
 
-                Thread.sleep(4500); // 5 секунд
+            WebElement username = driver.findElementByName("username");
+            username.sendKeys("89858191953");
 
-                driver.get("https://www.kinopoisk.ru/film/" + i + "/");
-                String str = driver.getPageSource();
-                if(str.contains("404. Страница не найден"))
-                {
-                    continue;
-                }
+            WebElement password = driver.findElementByName("password");
+            password.sendKeys("Hy65tgbvfr4");
 
-                if(str.contains("Нет подключения к Интернету")){
-                    i--;
-                    continue;
-                }
-                FileWriter wr = new FileWriter("C:\\Users\\azamat\\Desktop\\films\\film"+i+".txt");
-                wr.write(str);
-                wr.close();
+            password.submit();
+
+            Thread.sleep(3000);
+            driver.get("https://www.instagram.com/ussr_our_homeland/");
+
+            Thread.sleep(3000);
+
+            URL connection = new URL("https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c42.0.703.703a/s640x640/104474119_160587215657530_4854790570061227581_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=110&_nc_ohc=i1JuxpBQCHsAX_mvrDX&oh=b0e1838eb79b9fe830e54a6447e9e641&oe=5F18A3A7");
+            HttpURLConnection urlconn;
+            urlconn = (HttpURLConnection) connection.openConnection();
+            urlconn.setRequestMethod("GET");
+            urlconn.connect();
+            InputStream in = null;
+            in = urlconn.getInputStream();
+
+
+            File file = new File("C:\\Users\\azamat\\Desktop\\test.jpg");
+
+//            if(!file.exists()) {
+//                //Создаем его.
+//                file.createNewFile();
+//            }
+
+            OutputStream writer = new FileOutputStream(file);
+            byte buffer[] = new byte[1];
+            int c = in.read(buffer);
+            while (c > 0) {
+                writer.write(buffer, 0, c);
+                c = in.read(buffer);
             }
+            writer.flush();
+            writer.close();
+            in.close();
+            // https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c42.0.703.703a/s640x640/104474119_160587215657530_4854790570061227581_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=110&_nc_ohc=i1JuxpBQCHsAX_mvrDX&oh=b0e1838eb79b9fe830e54a6447e9e641&oe=5F18A3A7
+
+
+//            RemoteWebElement element= ((RemoteWebElement) driver.switchTo().defaultContent().findElement(yourElement));
+//            File temp = driver.getScreenshotAs(OutputType.FILE);
+//            Point point = element.getCoordinates().inViewPort();
+//            Dimension size = element.getSize();
+//            try {
+//                BufferedImage image = ImageIO.read(temp);
+//                return image.getSubimage(point.getX(), point.getY(), size.getWidth(), size.getHeight());
+//            } catch (IOException io) {
+//                System.out.println("Can't read screenshot in buffer");
+//                return null;
+//            }
+
+
+//            List<String> profileDirs = Lists.newArrayList("anonymous*webdriver-profile",
+//                    "userprofile*copy", "seleniumSslSupport*");
+//            File tmpDir = new File(System.getProperty("java.io.tmpdir"));
+//            FilenameFilter profileDirsFilter = new WildcardFileFilter(profileDirs);
+//            List<File> files = new ArrayList<>();
+//            files.addAll(Arrays.asList(tmpDir.listFiles(profileDirsFilter)));
+//            WebDriver driver = new FirefoxDriver();
+//            List<File> files1 = new ArrayList<>();
+//            files1.addAll(Arrays.asList(tmpDir.listFiles(profileDirsFilter)));
+//            files1.removeAll(files);
+//            System.out.print(files1.get(0)); //temp folder
+
+
+
+//            for(int i = 1071034; i <= 1100000; i++ ){
+//
+//                Thread.sleep(4000); // 5 секунд
+//
+//                driver.get("https://www.kinopoisk.ru/film/" + i + "/");
+//                String str = driver.getPageSource();
+//                if(str.contains("404. Страница не найден"))
+//                {
+//                    continue;
+//                }
+//
+//                if(str.contains("Нет подключения к Интернету")){
+//                    i--;
+//                    continue;
+//                }
+//                FileWriter wr = new FileWriter("C:\\Users\\azamat\\Desktop\\films\\film"+i+".txt");
+//                wr.write(str);
+//                wr.close();
+//            }
 
         }catch(Exception e){
             System.out.println(e.getMessage());
